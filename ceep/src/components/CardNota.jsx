537f48dceeb8bf6d285e10c15ed9e374a5def24a
danielemaraschin <danikmaraschin@gmail.com>
2021-05-27 15:56:47 +0200

    working with  components

---
+++ ceep/src/components/CardNota.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'; //imrc+TAB
+
+class CardNota extends Component { //CC+TAB
+    render() { 
+        return ( 
+
+        );
+    }
+}
+ 
+export default CardNota;
