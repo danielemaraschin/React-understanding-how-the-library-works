93322c725d3c46da219dfdc57aa0bec4935f93a1
danielemaraschin <danikmaraschin@gmail.com>
2021-05-27 07:27:02 +0200

    working with render() and components

--- ceep/src/components/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro.jsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React, { Component }  from "react";
 import { render } from "react-dom"
 
-export class FormularioCadastro extends Component{
+export class FormularioCadastro extends Component {
     render(){
         return (
             <form>
