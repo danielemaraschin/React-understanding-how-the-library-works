b6dd35b1913c76dfc67c7bbf8f7589cc99d7b55c
danielemaraschin <danikmaraschin@gmail.com>
2021-05-31 15:17:59 +0200

    working with css

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 
+
 class FormularioCadastro extends Component {
     render(){
         return (
-            <form >
-                <input type="text" placeholder="Título" />
-                <textarea placeholder="Escreva sua nota..." />
-                <button>Criar Nota</button>
+            <form className="form-cadastro" >
+                <input
+                    type="text" 
+                    placeholder="Título" 
+                    className="form-cadastro_input"
+                />
+                <textarea 
+                    placeholder="Escreva sua nota..." 
+                    className="form-cadastro_input"
+                />
+
+                <button  className="form-cadastro_input form-cadastro_submit">
+                    Criar Nota
+                </button>
             </form>
         );
     }
