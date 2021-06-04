9dd1b3a1cdafd29bc25e060006db83bddc3a7f72
danielemaraschin <danikmaraschin@gmail.com>
2021-06-04 11:04:18 +0200

    Working with props, element-child

--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import CardNota from "../CardNota";
 import "./estilo.css";
 class ListaDeNotas extends Component {
+
+  constructor(props){
+    super(props)
+  }
   render() {
     return (
       <ul className="lista-notas">
