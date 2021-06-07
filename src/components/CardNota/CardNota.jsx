1adfa0c9c11703006fad0fa157e102dfc07f39d0
danielemaraschin <danikmaraschin@gmail.com>
2021-06-07 17:35:19 +0200

    working with props

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -5,7 +5,7 @@ class CardNota extends Component {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
-          <h3 className="card-nota_titulo">{this.props.titulo}Título</h3>
+          <h3 className="card-nota_titulo">{this.props.titulo}</h3>
         </header>
         <p className="card-nota_texto">Escreva sua nota</p>
       </section>
