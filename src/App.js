0379d75350364fc7e0bc84ef73bda70c8f448114
danielemaraschin <danikmaraschin@gmail.com>
2021-06-07 17:19:06 +0200

    working with setState

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ class App extends Component {
     }
   }
 
-  criarNota(titulo, texto){
+  criarNota(titulo, texto){ //ao criar nova nota atribui um novo estado pra esse componente e renderiza
     const novaNota = {titulo, texto};
     const novoArrayNotas = [...this.state.notas, novaNota]
-    const novoEstado ={
-      notas: novoArrayNotas
+    const novoEstado ={     
+      notas: novoArrayNotas //ao inves de trabalhar com this.notas
     }
-    this.setState(novoEstado)
+    this.setState(novoEstado) //acessor que recebe o novo estado do componente -atualiza o estado que gerencia a renderizacao e renderiza o elemento
   }
   render() {
     console.log("render")
