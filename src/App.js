2371d1b745093e33e62859fbff1b5a1c2e79e697
danielemaraschin <danikmaraschin@gmail.com>
2021-06-07 17:33:49 +0200

    working with state

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ class App extends Component {
 
   constructor(){
     super();
-    this.notas = []
     this.state = {
-      notas: []
+      notas: [] //ao inves do this.notas = [] solto no constructor
     }
   }
 
   criarNota(titulo, texto){ //ao criar nova nota atribui um novo estado pra esse componente e renderiza
     const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas, novaNota]
-    const novoEstado ={     
+    const novoArrayNotas = [...this.state.notas, novaNota]//spreadOperator 
+    const novoEstado ={     //se tivesse mais props alem das notas poderia usar o spread operator aqui dentro tb
       notas: novoArrayNotas //ao inves de trabalhar com this.notas
     }
     this.setState(novoEstado) //acessor que recebe o novo estado do componente -atualiza o estado que gerencia a renderizacao e renderiza o elemento
   }
+
   render() {
     console.log("render")
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas}/> 
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
